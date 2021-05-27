b0b69091fe13924391d70fa0b399382a3a0eebd5
ramilamparo <ramilamparo@gmail.com>
2021-05-27 10:15:10 +0400

    Fix blank pages, attempt #1.

--- src/components/utils/Helmet.tsx
+++ src/components/utils/Helmet.tsx
@@ -90,35 +90,6 @@ export const Helmet = ({
 				},
 				...meta
 			]}
-			link={[
-				{
-					rel: "apple-touch-icon",
-					sizes: "180x180",
-					href: "/apple-touch-icon.png"
-				},
-				{
-					rel: "icon",
-					type: "image/png",
-					sizes: "32x32",
-					href: "/favicon-32x32.png"
-				},
-				{
-					rel: "icon",
-					type: "image/png",
-					sizes: "16x16",
-					href: "/favicon-16x16.png"
-				},
-				{
-					rel: "apple-touch-icon",
-					sizes: "180x180",
-					href: "/apple-touch-icon.png"
-				},
-				{
-					rel: "mask-icon",
-					href: "/safari-pinned-tab.svg",
-					color: "#455ad5"
-				}
-			]}
 		/>
 	);
 };
